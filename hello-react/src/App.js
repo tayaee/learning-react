a5d0756d856690b6fc80333ffe67b060ef321a2f
soonki ji <soonki.ji@gmail.com>
2021-02-07 17:31:31 -0500

    Completed p70

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = '리액트';
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>{name}야 안녕</h1>  
+      {name === '리액트' && <h1>리액트입니다</h1>}
+      <h2>잘 작동하니?</h2>
+    </>
   );
 }
 
