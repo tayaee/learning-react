2bcf640800fd55460b3f16ef086c9cfc28af92aa
soonki ji <soonki.ji@gmail.com>
2021-02-07 18:32:38 -0500

    p76 Comment format in JSX

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,27 +1,34 @@
 import './App.css';
+import * as React from "react";
 
 function App() {
   const name = '리액트';
   const style = {
     backgroundColor: 'black',
-        color: 'aqua',
-        fontSize: '40px',
-        fontWeight: 'bold',
-        padding: 16 // px    
+    color: 'aqua',
+    fontSize: '40px',
+    fontWeight: 'bold',
+    padding: 16 // px
   }
   return (
-    <>    
+    <>
+      {/*comment 1*/}
       <div style={style}>{name}1</div>
       <div style={{
+        // comment 2
         backgroundColor: 'gray',
         color: 'lightblue',
         fontSize: '40px',
         fontWeight: 'bold',
         padding: 16 // px
-      }}>{name}2</div>
-      <div className='react'>{name}3</div>
+      }}>{name}2
+      </div>
+      <div className='react'>{name}3 FIXME</div>
+      <input/>
+      /* This is not a comment */
+      <div>React version {React.version}</div>
     </>
-    );
+  );
 }
 
 export default App;
