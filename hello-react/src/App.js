2d9f9964e1a94771f82b4bcf5e190cc7d2697031
soonki ji <soonki.ji@gmail.com>
2021-02-08 01:12:19 -0500

    p155 Use 'ref' to access component or div objects (DOM objects). Scroll bar control demo.

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,11 +2,15 @@ import './App.css';
 import * as React from "react";
 import ScrollBox from "./ScrollBox";
 
-const App = () => {
-  return <div>
-    <ScrollBox/>
-    <button>Scroll down to the bottom</button>
-  </div>
-};
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => this.scrollBox = ref}/>
+        <button onClick={() => this.scrollBox.scrollToBottom()}>Scroll Down</button>
+      </div>
+    )
+  }
+}
 
 export default App;
--- hello-react/src/ScrollBox.js
+++ hello-react/src/ScrollBox.js
@@ -1,6 +1,12 @@
 import React, {Component} from "react";
 
 class ScrollBox extends Component {
+  scrollToBottom = () => {
+    console.log('scrollToBottom');
+    const {scrollHeight, clientHeight} = this.box;
+    this.box.scrollTop = scrollHeight - clientHeight;
+  };
+
   render() {
     const style = {
       border: '1px',
@@ -14,10 +20,14 @@ class ScrollBox extends Component {
       width: '300px',
       background: 'linear-gradient(white, black)',
     };
-    return <div style={style}>
-      <div style={innerStyle}>One</div>
-
-    </div>
+    return (
+      <div style={style}
+           ref={(ref) => {
+             this.box = ref
+           }}>
+        <div style={innerStyle}></div>
+      </div>
+    );
   }
 }
 
