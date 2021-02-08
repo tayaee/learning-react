d21af292abba2b29f9f6d2f839778cc9e9ef7b55
soonki ji <soonki.ji@gmail.com>
2021-02-08 14:52:26 -0500

    p179 Use shouldComponentUpdate() to render components conditionally.

--- hello-react/src/LifeCycleSample.js
+++ hello-react/src/LifeCycleSample.js
@@ -7,10 +7,18 @@ class LifeCycleSample extends Component {
   };
 
   onIncrementButtonClick = () => {
+    console.log('LifeCycleSample.onIncrementButtonClick() called: prev ' + this.state.number);
     this.setState({
       number: this.state.number + 1
     })
   };
+
+  shouldComponentUpdate = (nextProps, nextState) => {
+    // Do not render when current number is 4, i.e., 5 is not rendered. 1, 2, 3, 4, 6, 7, 8...
+    const decision = this.state.number % 10 !== 4;
+    console.log('LifeCycleSample.shouldComponentUpdate() called: ' + decision);
+    return decision;
+  }
 
   static getDerivedStateFromProps(nextProps, prevState) {
     console.log('LifeCycleSample.getDerivedStateFromProps() called');
