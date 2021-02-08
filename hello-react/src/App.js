b6bf95b53bd5750644553bd7115bba80e44c97aa
soonki ji <soonki.ji@gmail.com>
2021-02-07 22:41:50 -0500

    p128 Copying input text into state variable (demo).

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
 import * as React from "react";
-import Say from "./Say";
+import EventPractice from "./EventPractice";
 
 const App = () => {
-  return (
-    <>
-      <Say/>
-    </>
-  )
+  return <EventPractice/>
 };
 
 export default App;
---
+++ hello-react/src/EventPractice.js
@@ -0,0 +1,25 @@
+import React, {Component} from "react";
+
+class EventPractice extends Component {
+  state = {
+    message: ''
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Event Test</h1>
+        <input type='text'
+               name='message'
+               value={this.state.message}
+               placeholder='Enter message'
+               onChange={(e) => {
+                 console.log(e.target.name + '=' + e.target.value);
+                 this.setState({message: e.target.value})
+               }}/>
+      </div>
+    )
+  }
+}
+
+export default EventPractice;
