2e5e95ca64022248f241e983bb3db31c15bd3536
soonki ji <soonki.ji@gmail.com>
2021-02-07 23:44:26 -0500

    p139 Use 'form' variable to combine 'username', 'message' while state management (demo).

--- hello-react/src/EventPractice.js
+++ hello-react/src/EventPractice.js
@@ -1,37 +1,37 @@
-import React from "react";
-import {useState} from "react";
+import React, {useState} from "react";
 
 const EventPractice = () => {
-  const [username, setUsername] = useState('');
-  const [message, setMessage] = useState('');
-  const handleUsernameChange = (e) => setUsername(e.target.value);
-  const handleMessageChange = (e) => setMessage(e.target.value);
-  const handleClick = () => {
-    alert(username + ': ' + message);
-    setUsername('');
-    setMessage('');
+  const [form, setForm] = useState({
+    username: '',
+    message: ''
+  });
+  // FIXME: TypeError: form is not iterable => Use {, } instead of [, ].
+  const {username, message} = form;
+  const onChange = (e) => {
+    console.log(e.target.name + ': ' + e.target.value)
+    const nextForm = {
+      ...form,
+      [e.target.name]: e.target.value
+    };
+    setForm(nextForm)
   };
-  const handleKeyPress = (e) => {
+  const onClick = () => {
+    alert(username + ': ' + message);
+    setForm({username: '', message: ''});
+  };
+  const onKeyPress = (e) => {
     if (e.key === 'Enter') {
-      handleClick();
+      onClick();
     }
   };
   return (
     <div>
       <h1>Event Test</h1>
-      <input type='text'
-             name='username'
-             value={username}
-             placeholder='Enter username'
-             onChange={handleUsernameChange}/>
-      <input type='text'
-             name='message'
-             value={message}
-             placeholder='Enter message'
-             onChange={handleMessageChange}
-             onKeyPress={handleKeyPress}
+      <input type='text' name='username' value={username} placeholder='Enter username' onChange={onChange}/>
+      <input type='text' name='message' value={message} placeholder='Enter message' onChange={onChange}
+             onKeyPress={onKeyPress}
       />
-      <button onClick={handleClick}>Submit</button>
+      <button onClick={onClick}>Submit</button>
     </div>
   )
 };
