8ebbb166e00bb736533a956cfc4360998fe13b9a
soonki ji <soonki.ji@gmail.com>
2021-02-08 13:09:21 -0500

    p167 Adding new elements to a list using input box (demo).

--- hello-react/src/IterationSample.js
+++ hello-react/src/IterationSample.js
@@ -1,17 +1,36 @@
-import React from "react";
-import {useState} from "react";
+import React, {useState} from "react";
 
 const IterationSample = () => {
   const [names, setNames] = useState([
-    {id:1, name: 'spring'},
-    {id:2, name: 'summer'},
-    {id:3, name: 'fall'},
+    {id: 1, name: 'spring'},
+    {id: 2, name: 'summer'},
+    {id: 3, name: 'fall'},
   ]);
+  const [inputText, setInputText] = useState('');
+  const [nextId, setNextId] = useState(4);
   const nameList = names.map((e) => <li key={e.id}>{e.id} {e.name}</li>);
+  const onInputChange = (e) => {
+    setInputText(e.target.value)
+  };
+  const onButtonClick = () => {
+    const newNames = names.concat({id: nextId, name: inputText});
+    setNames(newNames);
+    setNextId(nextId + 1);
+    setInputText('');
+  };
+  const onKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      onButtonClick()
+    }
+  };
   return (
-    <ul>
-      {nameList}
-    </ul>
+    <>
+      <input value={inputText} onChange={onInputChange} onKeyPress={onKeyPress}/>
+      <button onClick={onButtonClick} >Add</button>
+      <ul>
+        {nameList}
+      </ul>
+    </>
   )
 };
 
