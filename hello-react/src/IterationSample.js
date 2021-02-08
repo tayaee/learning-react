e05ecbf1ca8be791d9b180af7c56edca7b54cf19
soonki ji <soonki.ji@gmail.com>
2021-02-08 12:54:40 -0500

    p165 Use user supplied id as iteration key (demo)

--- hello-react/src/IterationSample.js
+++ hello-react/src/IterationSample.js
@@ -1,9 +1,13 @@
 import React from "react";
+import {useState} from "react";
 
 const IterationSample = () => {
-  const names = ['spring', 'summer', 'fall', 'winter'];
-  // index.js:1 Warning: Each child in a list should have a unique "key" prop.
-  const nameList = names.map((e, index) => <li key={index}>{index} {e}</li>);
+  const [names, setNames] = useState([
+    {id:1, name: 'spring'},
+    {id:2, name: 'summer'},
+    {id:3, name: 'fall'},
+  ]);
+  const nameList = names.map((e) => <li key={e.id}>{e.id} {e.name}</li>);
   return (
     <ul>
       {nameList}
