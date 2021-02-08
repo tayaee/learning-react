af86e8df331a3c3fc303e5a3582653054ec0093c
soonki ji <soonki.ji@gmail.com>
2021-02-07 19:22:01 -0500

    p103 static defaultProps, static propTypes can be inside a class component.

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,21 +1,20 @@
-import React from "react";
+import React, {Component} from "react";
 import PropTypes from "prop-types";
 
-function MyComponent({name, children}) {
-  return <div>New Component! name={name}, children={children} p96 v2</div>;
-}
+class MyComponent extends Component {
+  static defaultProps = {
+    name: 'Default name'
+  };
 
-MyComponent.defaultProps = {
-  name: 'Default name'
-};
+  render() {
+    let {name, children} = this.props;
+    return <div>Switched to class component! name={name}, children={children} p96 v2</div>;
+  }
+}
 
 MyComponent.propTypes = {
   name: PropTypes.string,
   favoriteNumber: PropTypes.number.isRequired
 };
-// This is triggered on console tab if missing.
-// index.js:1 Warning: Failed prop type: The prop `favoriteNumber` is marked as required in `MyComponent`, but its value is `undefined`.
-// at MyComponent (http://localhost:3000/static/js/main.chunk.js:314:3)
-// at App (http://localhost:3000/static/js/main.chunk.js:168:1)
 
 export default MyComponent;
