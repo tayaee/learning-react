fac9dff9299a1d5e8cd3af172a66f147e72db493
soonki ji <soonki.ji@gmail.com>
2021-02-07 19:14:44 -0500

    p98 Type checking demo (PropTypes.string)

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -12,6 +12,10 @@ class App extends Component {
         <MyComponent name="React"/>
         <MyComponent/>
         <MyComponent>Children Text</MyComponent>
+        <MyComponent name={3}/>
+        {/*index.js:1 Warning: Failed prop type: Invalid prop `name` of type `number` supplied to `MyComponent`, expected `string`.*/}
+        {/*at MyComponent (http://localhost:3000/static/js/main.chunk.js:308:3)*/}
+        {/*at App (http://localhost:3000/main.c558e9edfd4bd8c2b638.hot-update.js:32:1)*/}
       </>
     )
   }
--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 function MyComponent({name, children}) {
   return <div>New Component! name={name}, children={children} p96 v2</div>;
@@ -6,6 +7,10 @@ function MyComponent({name, children}) {
 
 MyComponent.defaultProps = {
   name: 'Default name'
+};
+
+MyComponent.propTypes = {
+  name: PropTypes.string
 };
 
 export default MyComponent;
