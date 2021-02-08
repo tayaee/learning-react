e6a09c9fa21726a8273018153f3b02de2e9ea126
soonki ji <soonki.ji@gmail.com>
2021-02-07 19:17:32 -0500

    p100 isRequired demo while passing props.

--- hello-react/src/MyComponent.js
+++ hello-react/src/MyComponent.js
@@ -10,7 +10,12 @@ MyComponent.defaultProps = {
 };
 
 MyComponent.propTypes = {
-  name: PropTypes.string
+  name: PropTypes.string,
+  favoriteNumber: PropTypes.number.isRequired
 };
+// This is triggered on console tab if missing.
+// index.js:1 Warning: Failed prop type: The prop `favoriteNumber` is marked as required in `MyComponent`, but its value is `undefined`.
+// at MyComponent (http://localhost:3000/static/js/main.chunk.js:314:3)
+// at App (http://localhost:3000/static/js/main.chunk.js:168:1)
 
 export default MyComponent;
