3164212de09df172bce5c1fbe0a26fa2af669bd1
soonki ji <soonki.ji@gmail.com>
2021-02-08 20:59:10 -0500

    p194 useEffect() in functional component works the same way as componentDidMount / componentDidUpdate in class component (demo).

--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -1,8 +1,13 @@
 import React, {useState} from "react";
+import {useEffect} from "react";
 
 const Info = () => {
   const [name, setName] = useState('');
   const [nickname, setNickname] = useState('');
+  useEffect(() => {
+    console.log('useEffect() called. The same was as componentDidMount/componentDidUpdate in class component.')
+    console.log('name: ' + name + ', nickname: ' + nickname);
+  });
   const onNameChange = (e) => setName(e.target.value);
   const onNicknameChange = (e) => setNickname(e.target.value);
   return <div>
