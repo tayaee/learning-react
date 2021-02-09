91799503694a18e93c038e889e9b5fda676e907c
soonki ji <soonki.ji@gmail.com>
2021-02-08 22:29:21 -0500

    p208 Use useCallback() to optimize function creation.

--- hooks-tutorial/src/Average.js
+++ hooks-tutorial/src/Average.js
@@ -1,8 +1,7 @@
-import React, {useState} from "react";
-import {useMemo} from "react";
+import React, {useCallback, useMemo, useState} from "react";
 
 function getAverage(numList) {
-  console.log('This is optimized now.');
+  console.log('This is optimized now by useMemo().');
   if (numList.length === 0) return 0;
   if (numList.length === 1) return numList[0];
   let sum = numList.reduce((a, b) => a + b);
@@ -12,13 +11,13 @@ function getAverage(numList) {
 const Average = () => {
   const [numList, setNumList] = useState([]);
   const [value, setValue] = useState('');
-  const onChange = (e) => setValue(e.target.value);
-  const onClick = () => {
+  const onChange = useCallback(e => setValue(e.target.value), []);
+  const onClick = useCallback(() => {
     const newNumList = numList.concat(parseInt(value));
     console.log('numList: ', newNumList);
     setNumList(newNumList);
     setValue('');
-  };
+  }, [value, numList]);
   let avg = useMemo(() => getAverage(numList), [numList]);
   return <div>
     <input name={value} value={value} onChange={onChange}/>
