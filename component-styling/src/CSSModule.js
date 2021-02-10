3bf0cff1a5e594775cb17035f4e046d5cfc6ffc3
soonki ji <soonki.ji@gmail.com>
2021-02-09 23:38:36 -0500

    p240 CSSModule + classnames + SCSS (demo)

--- component-styling/src/CSSModule.js
+++ component-styling/src/CSSModule.js
@@ -1,6 +1,6 @@
 import React from "react";
 import classNames from 'classnames/bind';
-import styles from './CSSModule.module.css';
+import styles from './CSSModule.module.scss';
 
 const cx = classNames.bind(styles);
 
