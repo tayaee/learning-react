c6b8b7eec5ff86d41b254b837146bf5ba1fe76fc
soonki ji <soonki.ji@gmail.com>
2021-02-10 00:13:16 -0500

    p245 Understanding how styled.div`` or styled.button`` work. ES6 tagged template literal example.

--- component-styling/src/App.js
+++ component-styling/src/App.js
@@ -2,7 +2,19 @@ import './App.css';
 import React from "react";
 import StyledComponent from "./StyledComponent";
 
+function tagged(...args) {
+  console.log(args);
+}
+
 function App() {
+  console.log(`hello1 ${{foo: 'bar'}} ${() => 'world'}`);
+  // hello1 [object Object] () => 'world' <=== lost foo bar.
+
+  tagged`hello2 ${{foo: 'bar'}} ${() => 'world'}`;
+  // 0: (3) ["hello2 ", " ", "", raw: Array(3)]
+  // 1: {foo: "bar"}  <=== can access to the original expression here.
+  // 2: () => 'world'
+
   return (
     <div>
       <StyledComponent/>
