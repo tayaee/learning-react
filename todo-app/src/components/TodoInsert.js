a157f4c7773d184a44bd96253f9a2edf48dd56b7
soonki ji <soonki.ji@gmail.com>
2021-02-12 00:03:00 -0500

    p275 Keep input text in a state variable.

--- todo-app/src/components/TodoInsert.js
+++ todo-app/src/components/TodoInsert.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, {useCallback, useState} from "react";
 // TODO: Browse icons at https://react-icons.netlify.com/#/icons/md
 import {MdAdd} from "react-icons/md";
 import './TodoInsert.scss';
 
 const TodoInsert = () => {
   console.log('TodoInsert()');
+  const [value, setValue] = useState('');
+  const onChange = useCallback((e) => {
+    setValue(e.target.value)
+  }, []);
   return (
     <form className='TodoInsert'>
-      <input placeholder='Enter what to do'/>
+      <input value={value} onChange={onChange} placeholder='Enter what to do'/>
       <button type='submit'>
         <MdAdd/>
       </button>
