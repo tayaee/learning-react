1a080ffcf66dab02ae30f2985a06c197e388cf71
soonki ji <soonki.ji@gmail.com>
2021-03-07 17:25:38 -0500

    p333 A way to show the same component on multiple URLs.

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -10,11 +10,13 @@ const App = () => {
       <ul>
         <li><Link to={"/"}>Home</Link></li>
         <li><Link to={"/about"}>About</Link></li>
+        <li><Link to={"/info"}>Info</Link></li>
       </ul>
     </div>
     <hr/>
     <Route path="/" component={Home} exact={true}/>
     <Route path="/about" component={About}/>
+    <Route path="/info" component={About}/>
   </div>
 };
 
